d215acc829089a62c482a6c9b8dccea573efa8ed
Blitsik <blitsoljko@yandex.ru>
2026-03-20 22:05:34 +0500

    Update duels.lua

--- duels.lua
+++ duels.lua
@@ -1,3 +1,1 @@
-script_key = "knvuhkmpbpyasffntv9ttv1d"
 
-loadstring(game:HttpGet("https://api.getpolsec.com/scripts/hosted/2acb4704751ba034a6b059e56f85ccb9ef043a827cecdca420bc9584c917aa99.lua"))()
